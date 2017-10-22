aeaae23128b76237d90de0a8f66b5e5776a77075
AlexisMillete <salut.www@hotmail.com>
2017-10-22 14:36:09 -0400

    Ajout des commentaires des fonctions de conversion des mots

--- tp1.hs
+++ tp1.hs
@@ -6,7 +6,7 @@ firstConsonant = [(0,"g"),(1,"kk"),(2,"n"),(3,"d"),(4,"tt"),(5,"r"),(6,"m"),(7,"
 voyels = [(0,"a"),(1,"ae"),(2,"ya"),(3,"yae"),(4,"eo"),(5,"e"),(6,"yeo"),(7,"ye"),(8,"o"),(9,"wa"),(10,"wae"),(11,"oe"),(12,"yo"),(13,"u"),(14,"wo"),(15,"we"),(16,"wi"),(17,"yu"),(18,"eu"),(19,"ui"),(20,"i")]
 --Map contenant les dernieres consonnes
 secondConsonant = [(0,""),(1,"k"),(2,"k"),(3,"kt"),(4,"n"),(5,"nt"),(6,"nh"),(7,"t"),(8,"l"),(9,"lk"),(10,"lm"),(11,"lp"),(12,"lt"),(13,"lt"),(14,"lp"),(15,"lh"),(16,"m"),(17,"p"),(18,"pt"),(19,"t"),(20,"t"),(21,"ng"),(22,"t"),(23,"t"),(24,"k"),(25,"t"),(26,"p"),(27,"h")]
---Map de lettre et de map, qui eux contiennent une autre lettre et la lettre a changer
+--Map de lettre et de map, qui eux contiennent le numero d'une lettre et la lettre a changer
 replaceList = [(1,[(11,"g"),(2,"ngn"),(5,"ngn"),(6,"ngm"),(15,"k-k")]),(4,[(0,"n-g"),(5,"ll")]),(7,[(11,"d"),(2,"nn"),(5,"nn"),(6,"nm"),(16,"t-t")]),(8,[(11,"r"),(2,"ll"),(5,"ll")]),(16,[(5,"mn")]),(17,[(11,"b"),(2,"mn"),(5,"mn"),(6,"mm"),(17,"p-p")]),(19,[(11,"s"),(2,"nn"),(5,"nn"),(6,"nm"),(16,"t-t")]),(21,[(11,"ng-"),(5,"ngn")]),(22,[(11,"J"),(2,"nn"),(5,"nn"),(6,"nm")]),(23,[(11,"ch"),(2,"nn"),(5,"nn"),(6,"nm"),(16,"t-t")]),(25,[(2,"nn"),(5,"nn"),(6,"nm"),(16,"t-t")]),(27,[(11,"h"),(0,"k"),(2,"nn"),(3,"t"),(5,"nn"),(6,"nm"),(7,"p"),(9,"hs"),(12,"ch"),(18,"t")])]
 
 main = do
@@ -81,10 +81,17 @@ myLookup key ((x, y):list) =
        then (key,y)
        else myLookup key list
 
+-- Argument 1: Liste de liste de tuple de tuples (Int, String) représentant le texte divisé en phrases
+--
+-- Cette fonction prend la liste de phrase et retourne la liste de phrase transformée selon le tableau de conversion
 correctSentence :: [[((Int, String), (Int, String), (Int, String))]] -> [[(String, String, String)]]
 correctSentence [] = []
 correctSentence xs = map (\(list) -> (transformHangeul list)) xs
 
+-- Argument 1: Liste de tuple de tuples (Int, String) représentant une phrase
+--
+-- Cette fonction prend la phrase en parametre et tourne la phrase sous forme de liste de tupe de String,
+-- avec les mots transformés selon le tableau de de conversion
 transformHangeul :: [((Int, String), (Int, String), (Int, String))] -> [(String, String, String)]
 transformHangeul ((first1,second1,third1):(first2,second2,third2):list)
     | length list == 0 =
@@ -106,13 +113,26 @@ transformHangeul ((first1,second1,third1):(first2,second2,third2):list)
                 then (snd first1, snd second1, snd third1) : transformHangeul (word2:list)
                 else newWord : transformHangeul (((0,[]),second2,third2):list)
 
+-- Argument 1: Tuple de tuple de (Int, String) représentant un mot
+-- Argument 2: Tuple de tuple de (Int, String) représentant le mot suivant
+-- Argument 3: Liste de tuple de (Int, [(Int,String)]), représentant le tableau de conversion
+--
+-- Cette fonction passe au travers du tableau de conversion et compare la premiere valeur du tuple de la liste, à la 1e valeur
+-- du 3e tuple du 1er argument, s'il trouve une correspondantce, il appelle findMatch pour y trouver la lettre et retourne
+-- un tuple de String avec les 2 premieres valeurs du 1er argument et le string trouve par findMatch comme 3e valeur.
 replaceLetter :: ((Int, String), (Int, String), (Int, String)) -> ((Int, String), (Int, String), (Int, String)) -> [(Int,[(Int, String)])] -> (String, String, String)
-replaceLetter word1 word2 [] = tupleToString word1
+replaceLetter word1 word2 [] = tupleToTupleString word1
 replaceLetter (first1, second1, third1) (first2, second2, third2) ((number, letterList):list) =
     if fst third1 == number
         then (snd first1, snd second1, (findMatch third1 first2 letterList))
         else replaceLetter (first1, second1, third1) (first2, second2, third2) list
 
+-- Cette fonction prend en 1er argument un tuple qui représente la derniere lettre d'un mot, en 2e argument un tuple qui
+-- représente la 1e lettre du mot suivant et en 3e argument une liste de tuple (Int, String).
+--
+-- La fonction passe à travers la liste de tuple et vérifie si un tuple correspond au tuple du 2e arguement, s'il le trouve,
+-- il retourne le String du tuple qui correspond dans la liste.
+-- S'il n'en trouve pas, il retourne le string du tuple du 1er arguement
 findMatch :: (Int, String) -> (Int, String) -> [(Int, String)] -> String
 findMatch third _ [] = snd third
 findMatch third first ((number, letter):list) =
@@ -120,16 +140,21 @@ findMatch third first ((number, letter):list) =
         then letter
         else findMatch third first list
 
-tupleToString :: ((Int, String), (Int, String), (Int, String)) -> (String, String, String)
-tupleToString (first, second, third) = (snd first, snd second, snd third)
+-- Cette fonction prend un tuple de (Int, String) et retourne un tuple de String
+tupleToTupleString :: ((Int, String), (Int, String), (Int, String)) -> (String, String, String)
+tupleToTupleString (first, second, third) = (snd first, snd second, snd third)
 
+-- Cette fonction prend une liste de liste de tuple de string en argument et retourne les strings concaténé avec les points
+-- séparant les phrases.
 printListOfListWithSeperator :: [[(String, String, String)]] -> String
 printListOfListWithSeperator [] = []
 printListOfListWithSeperator (x:xs) = printListOfTuple x ++ "." ++ printListOfListWithSeperator xs
 
+-- Cette fonction prend une liste de tuple de string en argument et retourne les strings concaténé
 printListOfTuple :: [(String, String, String)] -> String
 printListOfTuple [] = []
 printListOfTuple (x:xs) = printTuple x ++ printListOfTuple xs
 
+-- Cette fonction prend un tuple de string en argument et retourne les strings concaténé
 printTuple :: (String, String, String) -> String
 printTuple (x, y, z) = x ++ y ++ z
